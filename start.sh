da1ceee25ffa23bcc5d6f7d48d5a0a4852040739
Sunil Purushottam Thakare <sunil@thakares.com>
2025-05-07 12:40:34 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,11 +3,11 @@ export DNS_BIND=0.0.0.0:53
 export DNS_ENABLE_IPV6=1
 export DNS_MAX_PACKET_SIZE=4096
 export DNS_DB_PATH=/var/dns-server/dns.db
-export DNS_NS_RECORDS=ns1.bzo.in.,ns2.bzo.in.
+export DNS_NS_RECORDS=ns1.yourdomain.tld.,ns2.yourdomain.tld.
 export DNS_AUTHORITATIVE=1
 export DNS_CACHE_TTL=300
 export RUST_LOG=info
-export DNS_DEFAULT_DOMAIN=bzo.in
-export DNS_DEFAULT_IP=60.254.61.33
+export DNS_DEFAULT_DOMAIN=yourdomain.tld
+export DNS_DEFAULT_IP=<YOUR-PUBLIC-IP4>
 
-exec /var/dns-server/dns_server
+exec /var/dns-server/nx9-dns_server
