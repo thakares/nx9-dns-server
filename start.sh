a07e90e9341927efa5089d0b433396904283bbf1
Sunil Purushottam Thakare <sunil@thakares.com>
2025-05-07 12:45:05 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 export DNS_BIND=0.0.0.0:53
 export DNS_ENABLE_IPV6=1
 export DNS_MAX_PACKET_SIZE=4096
-export DNS_DB_PATH=/var/dns-server/dns.db
+export DNS_DB_PATH=/var/nx9-dns-server/dns.db
 export DNS_NS_RECORDS=ns1.yourdomain.tld.,ns2.yourdomain.tld.
 export DNS_AUTHORITATIVE=1
 export DNS_CACHE_TTL=300
