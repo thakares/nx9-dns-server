30e6e99badc5ee9e898783a720f77cc41a998404
Sunil Purushottam Thakare <sunil@thakares.com>
2025-05-07 12:39:24 +0530

    Update soa-update.sh

--- soa-update.sh
+++ soa-update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-DB_PATH="/var/dns-server/dns.db"
-ZONE="bzo.in"
+DB_PATH="/var/nx9-dns-server/dns.db"
+ZONE="yourdomain.tld"
 
 # Get today's date and an incremental suffix
 DATE=$(date +%Y%m%d)
@@ -10,7 +10,7 @@ NEW_SERIAL="${DATE}${SERIAL_SUFFIX}"
 # Update SOA value
 sqlite3 "$DB_PATH" <<EOF
 UPDATE dns_records
-SET value = 'ns1.bzo.in hostmaster.bzo.in $NEW_SERIAL 10800 3600 604800 86400'
+SET value = 'ns1.yourdomain.tld hostmaster.yourdomain.tld $NEW_SERIAL 10800 3600 604800 86400'
 WHERE domain = '$ZONE' AND record_type = 'SOA';
 EOF
 
